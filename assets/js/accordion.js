2d64190488d3f51d87f86bf7ad361ffdb386dd47
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-01-18 16:12:29 +0530

    Reallign CSS, minor adjustments on liquid tags
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,57 +1,56 @@
+var accordion = (function() {
 
-var accordion = (function () {
+    var $accordion = $('.js-accordion');
+    var $accordion_header = $accordion.find('.js-accordion-header');
+    var $accordion_item = $('.js-accordion-item');
 
-  var $accordion = $('.js-accordion');
-  var $accordion_header = $accordion.find('.js-accordion-header');
-  var $accordion_item = $('.js-accordion-item');
+    // default settings 
+    var settings = {
+        // animation speed
+        speed: 400,
 
-  // default settings 
-  var settings = {
-    // animation speed
-    speed: 400,
+        // close all other accordion items if true
+        oneOpen: false
+    };
 
-    // close all other accordion items if true
-    oneOpen: false
-  };
+    return {
+        // pass configurable object literal
+        init: function($settings) {
+            $accordion_header.on('click', function() {
+                accordion.toggle($(this));
+            });
 
-  return {
-    // pass configurable object literal
-    init: function ($settings) {
-      $accordion_header.on('click', function () {
-        accordion.toggle($(this));
-      });
+            $.extend(settings, $settings);
 
-      $.extend(settings, $settings);
+            // ensure only one accordion is active if oneOpen is true
+            if (settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+                $('.js-accordion-item.active:not(:first)').removeClass('active');
+            }
 
-      // ensure only one accordion is active if oneOpen is true
-      if (settings.oneOpen && $('.js-accordion-item.active').length > 1) {
-        $('.js-accordion-item.active:not(:first)').removeClass('active');
-      }
+            // reveal the active accordion bodies
+            $('.js-accordion-item.active').find('> .js-accordion-body').show();
+        },
+        toggle: function($this) {
 
-      // reveal the active accordion bodies
-      $('.js-accordion-item.active').find('> .js-accordion-body').show();
-    },
-    toggle: function ($this) {
+            if (settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+                $this.closest('.js-accordion')
+                    .find('> .js-accordion-item')
+                    .removeClass('active')
+                    .find('.js-accordion-body')
+                    .slideUp()
+            }
 
-      if (settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
-        $this.closest('.js-accordion')
-          .find('> .js-accordion-item')
-          .removeClass('active')
-          .find('.js-accordion-body')
-          .slideUp()
-      }
-
-      // show/hide the clicked accordion item
-      $this.closest('.js-accordion-item').toggleClass('active');
-      $this.next().stop().slideToggle(settings.speed);
+            // show/hide the clicked accordion item
+            $this.closest('.js-accordion-item').toggleClass('active');
+            $this.next().stop().slideToggle(settings.speed);
+        }
     }
-  }
 })();
 
-$(document).ready(function () {
-  accordion.init({ speed: 300, oneOpen: true });
+$(document).ready(function() {
+    accordion.init({ speed: 300, oneOpen: true });
 });
 
-$(window).on('load', function () { 
-  $('#ldiv').delay(200).fadeOut('slow');
+$(window).on('load', function() {
+    $('#ldiv').fadeOut('slow');
 });
