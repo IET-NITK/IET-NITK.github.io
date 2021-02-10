1f13d496ea890d8a8ac55d76da7659f6fe7a6303
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-02-10 11:12:57 +0400

    Status Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-12DBVMVFZ2`,
       },
     },
     // `gatsby-plugin-feed`,
