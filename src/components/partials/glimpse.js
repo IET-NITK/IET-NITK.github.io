f0195b041713d983c45e08ef77ebdb0bfa8173a4
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-05-21 20:41:54 +0530

    Status Update: fixed #59

--- src/components/partials/glimpse.js
+++ src/components/partials/glimpse.js
@@ -1,34 +1,31 @@
 import React from "react"
 import { graphql, Link, StaticQuery } from "gatsby"
-import { getRandom, RenderAuthors } from "../helper"
+import { RenderAuthors } from "../helper"
 import { Splide, SplideSlide } from "@splidejs/react-splide"
 import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css"
 import _ from "lodash"
 
-const GlimpseCard = ({ title, authors, id, description, url, children }) => (
-  <Link
-    to={url}
-    class="card bg-primary text-white text-decoration-none"
-    style={{ height: "15em" }}
-  >
-    <div class="card bg-primary" style={{ height: "13em" }}>
-      <div class="card-body">
-        <h5 class="card-title">{title}</h5>
-        <h6 class="card-subtitle mb-2 text-muted text-decoration-none">
-          {id === "Blogs" ? "Blog " : id === "Events" ? "Events " : "Project "}
-          {authors ? (
-            <>
-              by
-              {RenderAuthors(authors, "text-muted")}
-            </>
-          ) : null}
-        </h6>
-        <p class="card-text ">{description}</p>
-        {children}
-      </div>
-    </div>
-  </Link>
-)
+function useWindowSize() {
+  const isSSR = typeof window !== "undefined"
+  const [windowSize, setWindowSize] = React.useState({
+    width: isSSR ? 1200 : window.innerWidth,
+    height: isSSR ? 800 : window.innerHeight,
+  })
+
+  function changeWindowSize() {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("resize", changeWindowSize)
+
+    return () => {
+      window.removeEventListener("resize", changeWindowSize)
+    }
+  }, [])
+
+  return windowSize
+}
 
 const getRandomThings = (arr, n, currentRoute) => {
   return _.sampleSize(
@@ -42,6 +39,7 @@ const getRandomThings = (arr, n, currentRoute) => {
   )
 }
 export const Glimpse = ({ currentRoute }) => {
+  const { width } = useWindowSize()
   return (
     <StaticQuery
       query={graphql`
@@ -92,99 +90,106 @@ export const Glimpse = ({ currentRoute }) => {
           }
         }
       `}
-      render={({ sigs, blogs, events, projects }) => (
-        <div className="bg-primary text-light pt-4 pb-4">
-          <div className="container">
-            <h5>
-              <strong>Glimpse @ IET NITK</strong>
-            </h5>
-            <div className="row">
-              <div className="col-lg-3 col-md-4  mt-5 mb-5">
-                {getRandomThings(sigs.nodes, 1, currentRoute).map((e, i) => (
-                  <Link
-                    to={"/sigs/" + e.name.toLowerCase()}
-                    key={i}
-                    class="card bg-primary text-white text-decoration-none"
-                    style={{ height: "15em" }}
-                  >
-                    <div class="card-body">
-                      <h5 class="card-title">{e.name}</h5>
-                      <p className="card-text">{e.description}</p>
-                    </div>
-                    {/* <Link to="/" class="card-footer text-muted">
+      render={({ sigs, blogs, events, projects }) => {
+        return (
+          <div className="bg-primary text-light pt-4 pb-4">
+            <div className="container">
+              <h5>
+                <strong>Glimpse @ IET NITK</strong>
+              </h5>
+              <div className="row">
+                <div className="col-lg-3 col-md-4  mt-5 mb-5">
+                  {getRandomThings(sigs.nodes, 1, currentRoute).map((e, i) => (
+                    <Link
+                      to={"/sigs/" + e.name.toLowerCase()}
+                      key={i}
+                      class="card bg-primary text-white text-decoration-none"
+                      style={{ height: "15em" }}
+                    >
+                      <div class="card-body">
+                        <h5 class="card-title">{e.name}</h5>
+                        <p className="card-text">{e.description}</p>
+                      </div>
+                      {/* <Link to="/" class="card-footer text-muted">
                       2 days ago
                     </Link> */}
-                  </Link>
-                ))}
-              </div>
-              <div className="col-lg-9 col-md-8  mt-5 mb-5">
-                <Splide
-                  style={{ height: "13em" }}
-                  options={{
-                    perPage: 1,
-                    gap: "2rem",
-                    type: "loop",
-                    keyboard: true,
-                    pauseonFocus: true,
-                    autoplay: true,
-                    rewind: true,
-                    perMove: 1,
-                    interval: 5000,
-                    arrows: false,
-                    pagination: false,
-                    hasAutoplayProgress: true,
-                  }}
-                >
-                  {getRandomThings(
-                    [...blogs.nodes, ...events.nodes, ...projects.nodes],
-                    20,
-                    currentRoute
-                  ).map((e, i) => {
-                    const id = e.id.split("_")[0]
-                    return (
-                      <SplideSlide style={{ height: "15em" }} key={i}>
-                        <div class="card bg-primary" style={{ height: "15em" }}>
-                          <Link
-                            to={
-                              id === "Projects"
-                                ? "/projects/" +
-                                  e.name.toLowerCase().split(" ").join("")
-                                : `/${id.slice(0, -1).toLowerCase()}/${e.route}`
-                            }
-                            class="card-body text-white text-decoration-none"
+                    </Link>
+                  ))}
+                </div>
+                <div className="col-lg-9 col-md-8  mt-5 mb-5">
+                  <Splide
+                    style={{ height: "13em" }}
+                    options={{
+                      perPage: width<800?1:2,
+                      gap: "2rem",
+                      type: "loop",
+                      keyboard: true,
+                      pauseonFocus: true,
+                      autoplay: true,
+                      rewind: true,
+                      perMove: 1,
+                      interval: 5000,
+                      arrows: false,
+                      pagination: false,
+                      hasAutoplayProgress: true,
+                    }}
+                  >
+                    {getRandomThings(
+                      [...blogs.nodes, ...events.nodes, ...projects.nodes],
+                      20,
+                      currentRoute
+                    ).map((e, i) => {
+                      const id = e.id.split("_")[0]
+                      return (
+                        <SplideSlide style={{ height: "15em" }} key={i}>
+                          <div
+                            class="card bg-primary"
+                            style={{ height: "15em" }}
                           >
-                            <h5 class="card-title">{e.name}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted text-decoration-none">
-                              {id === "Blogs"
-                                ? "Blog "
-                                : id === "Events"
-                                ? "Events "
-                                : "Project "}
-                            </h6>
-                            <div class="card-text ">
-                              {id === "Events"
-                                ? _.truncate(e.excerpt, {
-                                    length: 200,
-                                  })
-                                : null}
-                            </div>
-                          </Link>
-                          {e.authors ? (
-                            <div class="card-footer text-muted">
-                              By
-                              {RenderAuthors(e.authors, "text-muted")}
-                            </div>
-                          ) : null}
-                        </div>
-                      </SplideSlide>
-                    )
-                  })}
-                </Splide>
+                            <Link
+                              to={
+                                id === "Projects"
+                                  ? "/projects/" +
+                                    e.name.toLowerCase().split(" ").join("")
+                                  : `/${id.slice(0, -1).toLowerCase()}/${
+                                      e.route
+                                    }`
+                              }
+                              class="card-body text-white text-decoration-none"
+                            >
+                              <h5 class="card-title">{e.name}</h5>
+                              <h6 class="card-subtitle mb-2 text-muted text-decoration-none">
+                                {id === "Blogs"
+                                  ? "Blog "
+                                  : id === "Events"
+                                  ? "Events "
+                                  : "Project "}
+                              </h6>
+                              <div class="card-text ">
+                                {id === "Events"
+                                  ? _.truncate(e.excerpt, {
+                                      length: 200,
+                                    })
+                                  : null}
+                              </div>
+                            </Link>
+                            {e.authors ? (
+                              <div class="card-footer text-muted">
+                                By
+                                {RenderAuthors(e.authors, "text-muted")}
+                              </div>
+                            ) : null}
+                          </div>
+                        </SplideSlide>
+                      )
+                    })}
+                  </Splide>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )
+      }}
     />
   )
 }
