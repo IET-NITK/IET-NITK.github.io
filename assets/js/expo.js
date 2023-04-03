594b5f90e06feaf7775aa8e4b14a4dd58ff75bc8
M.B.SAI ADITYA <mbsaiaditya@gmail.com>
2023-04-03 17:03:52 +0530

    update

--- assets/js/expo.js
+++ assets/js/expo.js
@@ -64,8 +64,7 @@ var projects = [
   <h3 class="featurette-heading fw-normal lh-1"><b>Tetragram (Python based Static Site Generator)
   </b></h3> <br>
   <h5 style="color:grey"><b>CIPHER</b></h5>  <br>
-  <p style="max-width:100rem;font-size: 16px;color: grey;">The  Project would be focussed on creating a flask
-  based Static Site Generator(SSG). A static site generator is a tool that generates a full static HTML website based on raw data and a set of templates. Essentially, a static site generator automates the task of coding individual HTML pages and gets those pages ready to serve to users ahead of time. 
+  <p style="max-width:100rem;font-size: 16px;color: grey;">Tetragram is a content management system application designed to streamline the process of creating, editing, and publishing content. The goal of Tetragram is to make it easy for users to manage their content without requiring any technical expertise. By providing a user-friendly interface and powerful features, Tetragram enables users to create and publish high-quality content with ease.
   </p> <br>
   <p style="max-width:100rem;font-size: 16px;color: grey;">Built by Malavika Harikumar, Ayush Kumar Singh, Tarun S Hegde, Aditya Sankar and Aashish.</p> <br>
   <div class="row-md-12">
