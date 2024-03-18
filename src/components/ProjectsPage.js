633727603296f78598dfa759565deacad1015b82
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-03-18 19:30:51 +0530

    projects: added year wise filter

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -7,6 +7,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import { useMediaQuery } from "react-responsive";
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 const ProjectsPage = ({ projects, tag, img, sig }) => {
   const [currentPage, setCurrentPage] = useState(0);
@@ -14,6 +15,7 @@ const ProjectsPage = ({ projects, tag, img, sig }) => {
   const [selectedOption, setSelectedOption] = useState("2023");
   const projectsPerPage = bigScreen ? 8 : 6;
   const pathname = usePathname()
+  const router = useRouter()
 
   const handlePageClick = (data) => {
     setCurrentPage(data.selected);
@@ -34,7 +36,7 @@ const ProjectsPage = ({ projects, tag, img, sig }) => {
 
   const indexOfLastProject = (currentPage + 1) * projectsPerPage;
   const indexOfFirstProject = indexOfLastProject - projectsPerPage;
-  const currentProjects = projects.filter(project => project.project_sig.toLowerCase() == sig.toLowerCase()).slice(indexOfFirstProject, indexOfLastProject);
+  const currentProjects = projects.filter(project => project.project_sig.toLowerCase() == sig.toLowerCase()).filter(project => project.project_year == selectedOption).slice(indexOfFirstProject, indexOfLastProject);
   const pageCount = Math.ceil(projects.length / projectsPerPage);
 
   return (
@@ -44,44 +46,48 @@ const ProjectsPage = ({ projects, tag, img, sig }) => {
           {sig} Projects
         </div>
       </section>
-      <div className="flex gap-3 flex-col items-center mt-3">
-        <div display="flex">
-          <ul className="flex p-0 m-0 w-full bg-gray-200 rounded-lg list-none shadow-md">
-            {options.map((data, index) => {
-              console.log(selectedOption == data.name);
-              return (
-                <li className=" p-1 flex-1" key={index}>
-                  {selectedOption == data.name ? (
-                    <button
-                      className=" text-[rgba(255,242,255,1)] m-0 py-2 px-4 border-0 outline-0 w-full rounded-sm relative will-change-auto transition-colors duration-300 ease-in"
-                      onClick={() => {
-                        setSelectedOption(data.name);
-                      }}
-                    >
-                      <div className="absolute inset-0 bg-purple-800 rounded-md"></div>
-                      <p className="whitespace-nowrap text-base m-0 font-semibold relative z-10">
-                        {data.name}
-                      </p>
-                    </button>
-                  ) : (
-                    <button
-                      className=" text-purple-800 m-0 py-2 px-4 border-0 outline-0 w-full rounded-sm relative will-change-auto transition-colors duration-300 ease-in"
-                      onClick={() => {
-                        setSelectedOption(data.name);
-                      }}
-                    >
-                      <div className="absolute inset-0 rounded-md"></div>
-                      <p className="whitespace-nowrap text-base m-0 font-semibold relative z-10">
-                        {data.name}
-                      </p>
-                    </button>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
+      {currentProjects.length > 0 &&
+        <div className="flex gap-3 flex-col items-center mt-3">
+          <div display="flex">
+            <ul className="flex p-0 m-0 w-full bg-[#ebe5ec] border-[1px] border-[#71287e] rounded-lg list-none shadow-md">
+              {options.map((data, index) => {
+                console.log(selectedOption == data.name);
+                return (
+                  <li className="p-1 flex-1" key={index}>
+                    {selectedOption == data.name ? (
+                      <button
+                        className=" text-[rgba(255,242,255,1)] m-0 py-2 px-4 border-0 outline-0 w-full rounded-sm relative will-change-auto transition-colors duration-300 ease-in"
+                        onClick={() => {
+                          setSelectedOption(data.name);
+                          router.push(data.url)
+                        }}
+                      >
+                        <div className="absolute inset-0 bg-purple-800 rounded-md"></div>
+                        <p className="whitespace-nowrap text-base m-0 font-semibold relative z-10">
+                          {data.name}
+                        </p>
+                      </button>
+                    ) : (
+                      <button
+                        className=" text-purple-800 m-0 py-2 px-4 border-0 outline-0 w-full rounded-sm relative will-change-auto transition-colors duration-300 ease-in"
+                        onClick={() => {
+                          setSelectedOption(data.name);
+                          router.push(data.url)
+                        }}
+                      >
+                        <div className="absolute inset-0 rounded-md"></div>
+                        <p className="whitespace-nowrap text-base m-0 font-semibold relative z-10">
+                          {data.name}
+                        </p>
+                      </button>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      }
       <div className='py-6 p-5 md:p-8 grid justify-items-center gap-5 grid-cols-1 md:grid-cols-2 xl:px-20 lg:grid-cols-3 2xl:grid-cols-4 auto-rows-max relative' >
         {currentProjects.length === 0 && <div className="absolute text-center mt-20 text-2xl font-semibold">No projects found</div>}
         {currentProjects.map((project, index) => {
